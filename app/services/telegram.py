c8522c312b8076c104717f562d8e01bec39df08d
vladislav isayan <v.isayan@idaproject.com>
2025-04-20 11:01:56 +0500

    добавил папки

--- app/services/telegram.py
+++ app/services/telegram.py
@@ -159,7 +159,6 @@ class TgClient:
                     if isinstance(target_chat_id, str) and not target_chat_id.startswith('@'):
                         target_chat_id = f"@{target_chat_id}"
 
-                print(target_chat_id)
                 await self.client.send_document(
                     chat_id=target_chat_id,
                     document=str(backup_path),
